a34ecc40be85a7dd3ed66591a907a73e09f97161
ThalesTrombim <thalestr@outlook.com>
2021-03-30 21:32:43 -0300

    commit heroku

--- src/Components/Pages/Cadastro/Categoria/index.js
+++ src/Components/Pages/Cadastro/Categoria/index.js
@@ -35,18 +35,19 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
-    fetch(URL);
-    .then(async (respostaDoServer) =>{
-      if(respostaDoServer.ok) {
-        const resposta = await respostaDoServer.json();
-        setCategorias(resposta);
-        return; 
-      }
-      throw new Error('Não foi possível pegar os dados');
-     })
-  }    
-}, []);
+    if (window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias';
+      fetch(URL)
+        .then(async (respostaDoServer) => {
+          if (respostaDoServer.ok) {
+            const resposta = await respostaDoServer.json();
+            setCategoria(resposta);
+            return;
+          }
+          throw new Error('Não foi possível pegar os dados');
+        });
+    }
+  }, []);
 
   return (
     <PageDefault>
